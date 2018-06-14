e0c25f87b0cd7f5e2179fdf1696cd1bcfd18fa67
jflick00 <jflick2389@gmail.com>
2018-06-14 16:27:12 -0400

    Updated bamazonCustomer.js file.

--- bamazon.sql
+++ bamazon.sql
@@ -7,12 +7,12 @@ CREATE TABLE products(
 id INT NOT NULL AUTO_INCREMENT,
 product_name VARCHAR(100) NOT NULL,
 department_name VARCHAR(100) NOT NULL,
-price DECIMAL(65.2) NULL,
-stock_quantity INT NULL,
+price DECIMAL(10,2) NOT NULL,
+stock_quantity INT NOT NULL,
 PRIMARY KEY (id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Ready Player One", "Movies and TV", 24.99, 500);
+VALUES ("Ready Player One", "Movies and TV", 24.99, 500), ("Echo (Smart speaker with Alexa)", "Echo and Alexa", 79.99, 150), ("Sony KD70X69OE 70-Inch 4K Ultra HD Smart LED TV (2017 Model)", "TV and Video", 998.00, 100), ("Apple 13 inch MacBook Air (2017 Version) 1.8GHz Core i5 CPU, 8GB RAM, 256GB SSD", "Computers", 1035.00, 50), ("Fire HD 8 Tablet with Alexa", "Computers", 59.99, 100), ("Playstation 4 Slim 500GB Console", "Video Games", 289.95, 130), ("Xbox One S 500GB Console", "Video Games", 199.00, 150), ("Nintendo Switch", "Video Games", 299.00, 80), ("Oculus Rift - Virtual Reality Headset", "Video Games", 499.00, 60), ("Black Panther", "Movies and TV", 17.00, 500);
 
-SELECT * FROM products
+SELECT * FROM products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,10 @@
+//  Require all npm packages
 require("dotenv").config();
 
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-// create the connection information for the sql database
+// Create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -12,88 +13,74 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
+//  Initialize connection
 connection.connect(function(err) {
+  //  if error...
   if (err) throw err;
+  //  if no error, run itemsForsale function
   itemsForsale();
-  setTimeout(displayMessages, 1000);
 });
 
 function itemsForsale() {
-  
+  //  Connecting to sequel database and selecting table (products)
   connection.query("SELECT * FROM products", function(err, res) {
+    
+    //  Console log column headers
     console.log("-----------------------------------");
-    console.log(" ID | Product Name | Price ");  
+    console.log(" ID\tProduct Name\tPrice ");  
     console.log("-----------------------------------");
     
+    //  For-loop to display id, product name, and price from table
     for (var i = 0; i < res.length; i++) {
-      console.log(" " + res[i].id + " | " + res[i].product_name + " | " + res[i].price.toFixed(2));
+      console.log(" " + res[i].id + "\t" + res[i].product_name + "\t" + res[i].price.toFixed(2));
     }
     console.log("-----------------------------------");
   });
+  
+  //  Run displayMessages function
+  displayMessages();
 }
 
-function displayMessages() {
-  connection.query("SELECT * FROM products", function(res) {
+function displayMessages(res) {
 
-  // prompt for info about the item being put up for auction
+  //  Prompt user to see what item they would like to purchase
   inquirer
     .prompt([
       {
         name: "message1",
         type: "input",
         message: "Please type in the id of the product you would like to purchase.",
-        // validate: function(answer) {
-        //   for (var i=0; i < res.length; i++) {
-        //       if (answer.message1 !== res.id[i]) {
-        //         return true;
-        //       }
-        //       return false;
-        //   }
-        // }
-        
-      },
-      {
-        name: "message2",
-        type: "input",
-        message: "How many units of the product would you like to purchase?",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
+      }
+    ]).then(function(answer) {
+      var correct = false;
+      for (var i=0; i < res.length; i++) {
+        if(res[i].product_name == answer.message1) {
+          correct = true;
+          var product = answer.message1;
+          var id=i;
+          inquirer.prompt({
+            name: "message2",
+            type: "input",
+            message: "How many units would you like to purchase?",
+            validate: function(value) {
+              if(isNaN(value)==false) {
+                  return true;
+              } else {
+                  return false;
+              }
+            }
+          }).then(function(answer) {
+            if((res[id].stock_quantity-answer.message2) > 0) {
+              connection.query("UPDATE products SET stock_quantity='"+(res[id].stock_quantity-answer.message2+"' WHERE product_name='"+product+"'", function() {
+                console.log("Product Bought!");
+                itemsForsale();
+              }))
+            } else {
+                console.log("Not enough in stock, sorry!");
+                displayMessages(res);
+            }
+          })
         }
       }
-    ])
-    .then(function(answer) {
-      // // get the information of the chosen item
-      var chosenItem;
-      for (var i = 0; i < res.length; i++) {
-        if (res[i].id === answer.message1) {
-          chosenItem = res1[i];
-        }
-      }
-
-      // determine if there is enough stock
-      if (chosenItem.stock_quantity > parseInt(answer.message2)) {
-        connection.query(
-          "UPDATE products SET ? WHERE ?",
-          
-            {
-              stock_quantity: answer.message2
-            },
-           
-          function(error) {
-            if (error) throw err;
-            console.log("Order placed successfully!");
-            itemsForsale();
-          }
-        );
-      }
-      else {
-        console.log("Insufficient quantity!");
-        itemsForsale();
-      }
-    });
-  })
+    })
 }
-
